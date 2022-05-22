cc9e4987f3cd80cd82e92c674cb8127958af6425
Nikhil Sharma <nikhil.harsh.sharma@gmail.com>
2022-05-22 23:11:23 +0530

    feat: added data for videos, categories and user

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -1,4 +1,4 @@
-import { v4 as uuid } from "uuid";
+import { v4 as uuid } from 'uuid';
 
 /**
  * Category Database can be added here.
@@ -8,20 +8,37 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "Computer Programming",
-    description:
-      "Computer programming is the process of designing and building an executable computer program to accomplish a specific computing",
+    categoryName: 'all',
+    description: 'Collection of all videos',
   },
   {
     _id: uuid(),
-    categoryName: "Frontend Development",
+    categoryName: 'spirituality',
     description:
-      "Front-end web development, also known as client-side development is the practice of producing HTML, CSS and JavaScript for a website or Web Application",
+      'Spirituality is a broad concept with room for many perspectives. In general, it includes a sense of connection to something bigger than ourselves, and it typically involves a search for meaning in life',
   },
   {
     _id: uuid(),
-    categoryName: "Backend Development",
+    categoryName: 'meditation',
     description:
-      "Backend Development is also known as server-side development. It is everything that the users don't see and contains behind-the-scenes activities that occur when performing any action on a website.",
+      'Meditation is a practice in which an individual uses a technique – such as mindfulness, or focusing the mind on a particular object, thought, or activity – to train attention and awareness, and achieve a mentally clear and emotionally calm and stable state.',
+  },
+  {
+    _id: uuid(),
+    categoryName: 'depression',
+    description:
+      'Depression (major depressive disorder) is a common and serious medical illness that negatively affects how you feel, the way you think and how you act. ',
+  },
+  {
+    _id: uuid(),
+    categoryName: 'anxiety',
+    description:
+      'Anxiety is a feeling of fear, dread, and uneasiness. It might cause you to sweat, feel restless and tense, and have a rapid heartbeat. It can be a normal reaction to stress.',
+  },
+  {
+    _id: uuid(),
+    categoryName: 'better-self',
+    description:
+      'This genre is an umbrella term which would include all the things that would help us to be a better verion of ourself',
   },
 ];
