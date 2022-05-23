3e3221c43616733dcf84961a69b7de7ee0a03622
Nikhil Sharma <nikhil.harsh.sharma@gmail.com>
2022-05-24 02:27:40 +0530

    feat: added router, theme-context and video-context

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import './index.css';
 import App from './App';
 import { makeServer } from './server';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+import { VideoProvider, ThemeProvider } from './context';
+
 // Call make Server
 makeServer();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <VideoProvider>
+      <ThemeProvider>
+        <Router>
+          <App />
+        </Router>
+      </ThemeProvider>
+    </VideoProvider>
   </React.StrictMode>
 );
