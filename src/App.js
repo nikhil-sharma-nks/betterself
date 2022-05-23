7a08db6cf47b06bf994957c1d18c2e45f71013cd
Nikhil Sharma <nikhil.harsh.sharma@gmail.com>
2022-05-24 02:20:26 +0530

    styles: add variable and default

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.scss';
-
+import { Navbar, Sidebar } from './components';
+import RoutesContainer from './routes/RoutesContainer';
+import { useTheme } from './context';
 function App() {
-  return <div className='App'>Working</div>;
+  const { theme } = useTheme();
+  return (
+    <div className={`App theme-${theme}`}>
+      <Navbar />
+      <div className='app-body'>
+        <Sidebar />
+        <RoutesContainer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
