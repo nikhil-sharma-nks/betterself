3533353f98adad9c23e378e3e342496ba92c6b94
Nikhil Sharma <nikhil.harsh.sharma@gmail.com>
2022-05-25 01:18:33 +0530

    feat: added authenticated routes to app

--- src/routes/RoutesContainer.jsx
+++ src/routes/RoutesContainer.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Home } from '../pages';
+import { Home, Login, Signup, Likes } from '../pages';
+import AuthenticatedRoutes from './AuthenticatedRoutes';
+
 import Mockman from 'mockman-js';
 
 const RoutesContainer = () => {
@@ -8,7 +10,13 @@ const RoutesContainer = () => {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/home' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
       <Route path='/mock' element={<Mockman />} />
+      <Route path='/' element={<AuthenticatedRoutes />}>
+        <Route path='/likes' element={<Likes />} />
+        {/* <Route path='/wishlist' element={<Wishlist />} /> */}
+      </Route>
     </Routes>
   );
 };
